6b8b744c4ae76e50ffbebe1bc8bd4bcad3d5892a
Sacha Best <sachabest@outlook.com>
2014-02-07 21:36:58 -0500

    More data management progress

---
+++ Unity/Assets/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class DataManager : MonoBehaviour {
+
+    public const string websiteToCheck = "http://google.com";
+    public JSONObject downloadedJSON;
+    private List<FallingSite> downloadedSites;
+    private float downloadProgress;
+    public bool downloading;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+    void startHTTPCheck()
+    {
+
+    }
+    public void decodeJSON(string url)
+    {
+        downloading = true;
+        WWW jsonPage = new WWW(url);
+        while (!jsonPage.isDone)
+        {
+            downloadProgress = jsonPage.progress;
+        }
+        downloading = false;
+        string json = System.Text.Encoding.Default.GetString(jsonPage.bytes);
+        downloadedJSON = new JSONObject(json);
+        
+        foreach (JSONObject jsonSite in (downloadedJSON.list)) {
+            FallingSite toAdd = new FallingSite();
+            toAdd.siteURL = jsonSite.keys[0];
+            toAdd.value = int.Parse(jsonSite.keys[1]);
+            toAdd.mass = int.Parse(jsonSite.keys[2]);
+            downloadedSites.Add(toAdd);
+        }
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/UserLoginPrompter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserLoginPrompter : MonoBehaviour {
+
+    private string logvinURL;
+    private string username, password;
+    public UILabel usernameLabel, passwordLabel;
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+    public void processLogin()
+    {
+        this.username = usernameLabel.text;
+        this.password = passwordLabel.text;
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
