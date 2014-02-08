f26daeffd9daae511e871afd96c5b82358d77a97
Sacha Best <sachabest@outlook.com>
2014-02-07 20:25:21 -0500

    Unity Setup
    Adding basic frameworks and GameObject setup

---
+++ Unity/Assets/FallingSite.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingSite : MonoBehaviour {
+
+    public float value { get; set; }
+    public float mass { get; set; }
+    public string siteURL { get; set; }
+    public int id;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity/Assets/SiteCollector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SiteCollector : MonoBehaviour {
+
+    private int numSites;
+    public float value;
+    private List<FallingSite> sites;
+
+	// Use this for initialization
+	void Start () {
+        sites = new List<FallingSite>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void addSite(FallingSite site)
+    {
+        sites.Add(site);
+        numSites++;
+        value += site.value;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        FallingSite site = contact.otherCollider.gameObject.GetComponent<FallingSite>();
+        if (site != null)
+        {
+            this.addSite(site);
+            /* Need to also update the collider on this object to include the bounds of
+             * the recently caught site */
+
+        }
+    }
+}
